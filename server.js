486e74ba16363481eef02ed016c4d7ef4403e67f
Joseph Wang <josephwang1993@gmail.com>
2018-08-07 16:01:40 -0400

    Ready for build

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/client/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
 
 let onlineUsers = {}
 
